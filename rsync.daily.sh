9baa703ca8670907ec1fed804e8bf5ac3d477cba
404 pnf <baixiaoyu@gmail.com>
2010-11-04 13:39:01 +0800

    add rsync cmd to backup /home/apache/apps and /home/myapp/

--- rsync.daily.sh
+++ rsync.daily.sh
@@ -1,4 +1,11 @@
 
 # bxy: do this sync daily without --delete, would ranther err on the side of a bloated backup 
 # bxy: backup /var/www the whole directory because static.files is in that dir
-rsync -av --exclude-from="/backup/rsync.exclude.rules" /var/www/ /backup/www.html.backup/
+rsync -av --exclude-from="/backup/rsync.exclude.rules" /home/apache/data/ /backup/www.html.backup/
+
+# bxy: notice the ending slash of source does matter
+# e.g  /home/apps/ /backup/ wouldn't create a apps directory in destination
+# therefore mess up the /backup/ directory structure
+rsync -av --exclude-from="/backup/rsync.exclude.rules" /home/apache/apps/ /backup/app.backup/apps/
+rsync -av --delete --exclude-from="/backup/rsync.exclude.rules" /home/myapp/ /backup/app.backup/myapp/
+
--- rsync.monthly.sh
+++ rsync.monthly.sh
@@ -1,3 +1,5 @@
 # bxy: do this twice a month with --delete to remove the unnecessary files
-rsync -av --delete --exclude-from="/backup/rsync.exclude.rules" /var/www/ /backup/www.html.backup/
+rsync -av --delete --exclude-from="/backup/rsync.exclude.rules" /home/apache/data/ /backup/www.html.backup/
+rsync -av --delete --exclude-from="/backup/rsync.exclude.rules" /home/apache/apps/ /backup/app.backup/apps/
+rsync -av --delete --exclude-from="/backup/rsync.exclude.rules" /home/myapp/ /backup/app.backup/myapp/
 
