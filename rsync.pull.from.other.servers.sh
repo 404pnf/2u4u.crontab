4036b7d206851a79b52b0030df9de9cfa436befe
404 pnf <baixiaoyu@gmail.com>
2010-11-11 13:16:01 +0800

    rsync 223:/backup/ to /backup/

---
+++ rsync.pull.from.other.servers.sh
@@ -0,0 +1,11 @@
+
+# bxy: do this sync daily without --delete, would ranther err on the side of a bloated backup 
+# bxy: backup /var/www the whole directory because static.files is in that dir
+
+# pull /backup/ from 223 to here
+rsync -av --delete --exclude-from="/backup/rsync.exclude.rules" 223:/backup/ /backup/223.backup/
+
+# bxy: notice the ending slash of source does matter
+# e.g  /home/apps/ /backup/ wouldn't create a apps directory in destination
+# therefore mess up the /backup/ directory structure
+
